1425c1b6cf2ef118ce86e8e403e924d6ab2e8854
daniel-ferreira-da-costa <danielferreiradacosta015@gmail.com>
2023-02-25 04:53:15 -0300

    implementando CRUD - Aula 3 - Topicos 1

--- src/main/java/br/unitins/resource/ComputadorResource.java
+++ src/main/java/br/unitins/resource/ComputadorResource.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import br.unitins.model.Computador;
@@ -14,20 +17,67 @@ import br.unitins.model.Computador;
 // Retorna todos os objetos do banco
 @Path("/Computador")
 public class ComputadorResource {
-    @GET
-    @Path("/todos")
-    @Produces(MediaType.APPLICATION_JSON)
-     public List<Computador> getAllComputador(){
-        return Computador.findAll().list();
-     }
 
-     @POST
-     @Produces(MediaType.APPLICATION_JSON)
-     @Consumes(MediaType.APPLICATION_JSON)
-     @Transactional
-     public Computador insert(Computador computador){
+   @GET
+   @Path("/todos")
+   @Produces(MediaType.APPLICATION_JSON)
+   public List<Computador> getAllComputador() {
+      return Computador.findAll().list();
+   }
+
+   // Consultar por id
+   @GET
+   @Path("/{id}")
+   @Consumes(MediaType.APPLICATION_JSON)
+   public Computador searchJogo(@PathParam("id") Long id) {
+       return Computador.findById(id);
+   }
+
+   //insere pc
+   @POST
+   @Produces(MediaType.APPLICATION_JSON)
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Transactional
+   public Computador insert(Computador computador) {
+      //adiciona o computador no banco de dados
       computador.persist();
-        return computador;
 
-     }
+      return computador;
+
+   }
+
+   //altera pc
+   @PUT
+   @Path("/{id}")
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Produces(MediaType.APPLICATION_JSON)
+   @Transactional
+   public Computador uptadte(@PathParam("id") Long id, Computador computador) {
+
+      Computador entity = Computador.findById(id);
+
+      entity.setNome(computador.getNome());
+      entity.setPecas(computador.getPecas());
+      entity.setValor(computador.getValor());
+      entity.setPerifericos(computador.getPerifericos());
+      entity.setEstoque(computador.getEstoque());
+
+       return entity;
+   }
+
+   //deletar pc
+   @DELETE
+   @Path("/{id}")
+   @Produces(MediaType.APPLICATION_JSON)
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Transactional
+   public Computador deleteJogo(@PathParam("id") Long id, Computador jogo) {
+      Computador computadorDeletado = Computador.findById(id);
+         
+      computadorDeletado.delete();
+ 
+      return computadorDeletado;
+   }
+
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,7 @@
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+
+insert into Computador (id, nome, pecas, perifericos, valor, estoque) values(nextval('hibernate_sequence'), 'Low desktop', 'processador i3 10105, placa mae LGA 1200, Memoria RAM 8gb DDR4, fonte 500w, placa de video gtx 1050ti', 'kit mouse, teclado e headset Redragon, monitor full hd', 3500, 30);
+insert into Computador (id, nome, pecas, perifericos, valor, estoque) values(nextval('hibernate_sequence'), 'Medium desktop', 'processador i5 12400F, placa mae LGA 1700, Memoria RAM 16gb DDR4, fonte 600w, placa de video rtx 2060', 'kit mouse, teclado e headset Redragon, monitor full hd', 7000, 30);
+insert into Computador (id, nome, pecas, perifericos, valor, estoque) values(nextval('hibernate_sequence'), 'High desktop', 'processador i9 13900k, placa mae LGA 1700, Memoria RAM 32gb DDR5, fonte 1200w, placa de video rtx 4090ti', 'kit mouse, teclado e headset Redragon, monitor 4k', 18000, 30);
